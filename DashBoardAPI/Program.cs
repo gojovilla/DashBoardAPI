f7ab36e61947abec6924946dd0ce5b5821bc16fd
sonu2 <rajbhar9969@gmail.com>
2023-12-20 20:56:54 +0530

    changes

--- DashBoardAPI/Program.cs
+++ DashBoardAPI/Program.cs
@@ -43,7 +43,7 @@ app.UseRouting();
 //});
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()|| app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
